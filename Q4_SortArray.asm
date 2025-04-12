baf3ca0b847269ea663d8fc0b684b802c69dc4da
Seontae <hitu8931@yonsei.ac.kr>
2025-04-12 22:30:46 +0900

    Q4

--- Q4_SortArray.asm
+++ Q4_SortArray.asm
@@ -12,100 +12,134 @@ space:
 
 n:
   .align 2
-  .space 4 # <-- capable of storing a 32-bit unsigned integer
+  .space 4        # Capable of storing a 32-bit unsigned integer
 
 array:
   .align 2
-  .space 4096 # <-- capable of storing up to 1024 32-bit integers
+  .space 4096     # Capable of storing up to 1024 32-bit integers
 
 ################################################################################
 
 .text
 
-# $a0 = n, $a1 = &array[0]
+# print_array: prints n integers from an array
+# $a0 = n, $a1 = address of array[0]
 print_array:
-  move $t0, $a0
-  li $t1, 0
-  move $t2, $a1
+  move $t0, $a0       # $t0 holds the number of elements to print
+  li   $t1, 0         # $t1 is the index counter
+  move $t2, $a1       # $t2 points to the current array element
 print_array_loop:
-  beq $t1, $t0, print_array_return
-  li $v0, 1
-  lw $a0, ($t2)
+  beq  $t1, $t0, print_array_return  # If index equals n, finish printing
+  li   $v0, 1         # syscall for print_int
+  lw   $a0, 0($t2)    # load the current array element
   syscall
-  li $v0, 4
-  la $a0, space
+  li   $v0, 4         # syscall for print_string
+  la   $a0, space
   syscall
-  addi $t1, $t1, 1
-  addi $t2, $t2, 4
-  j print_array_loop
+  addi $t1, $t1, 1    # index++
+  addi $t2, $t2, 4    # move pointer to the next integer (each int is 4 bytes)
+  j    print_array_loop
 print_array_return:
-  li $v0, 4
-  la $a0, newline
+  li   $v0, 4
+  la   $a0, newline
   syscall
-  jr $ra
+  jr   $ra
 
-# $a0 = n, $a1 = &array[0]
+# sort_array: sorts an array of 32-bit integers in ascending order using bubble sort.
+# $a0 = n (number of elements), $a1 = base address of array.
 sort_array:
-################################################################################
-# FIXME
+  # Save number of elements in $t2
+  move $t2, $a0        # $t2 = n
 
-  nop
+  li   $t0, 0          # Outer loop counter, i = 0
+outer_loop:
+  # Outer loop runs while i < n-1
+  sub  $t3, $t2, 1     # $t3 = n - 1
+  bge  $t0, $t3, sort_end  # if i >= n-1, we're done
 
-# FIXME
-################################################################################
-  jr $ra
+  li   $t1, 0          # Inner loop counter, j = 0
+inner_loop:
+  # Inner loop runs for j < (n - i - 1)
+  sub  $t4, $t2, $t0   # $t4 = n - i
+  addi $t4, $t4, -1    # $t4 = n - i - 1
+  bge  $t1, $t4, inner_end  # if j >= (n-i-1), exit inner loop
+
+  # Compute address for array[j]: base + (j * 4)
+  sll  $t6, $t1, 2     # $t6 = j * 4
+  add  $t5, $a1, $t6   # $t5 = address of array[j]
+  lw   $t7, 0($t5)     # $t7 = array[j]
+
+  # Compute address for array[j+1]: base + ((j+1) * 4)
+  addi $t8, $t1, 1     # $t8 = j + 1
+  sll  $t8, $t8, 2     # $t8 = (j+1) * 4
+  add  $t9, $a1, $t8   # $t9 = address of array[j+1]
+  lw   $t8, 0($t9)     # $t8 = array[j+1]
+
+  # Compare array[j] and array[j+1]; swap if out-of-order
+  ble  $t7, $t8, no_swap  # if array[j] <= array[j+1], no swap needed
+
+  # Swap:
+  sw   $t8, 0($t5)     # store array[j+1] into array[j]
+  sw   $t7, 0($t9)     # store array[j] into array[j+1]
+
+no_swap:
+  addi $t1, $t1, 1     # inner loop: j++
+  j    inner_loop
+
+inner_end:
+  addi $t0, $t0, 1     # outer loop: i++
+  j    outer_loop
+
+sort_end:
+  jr   $ra             # Return from sort_array
 
 .globl main
 main:
-  # $s0 = $ra
+  # Save $ra in $s0
   move $s0, $ra
 
-  # print_string(str1); read_int (--> $v0); *n = $v0
-  li $v0, 4
-  la $a0, str1
+  # Prompt for number of integers to sort and read it
+  li   $v0, 4
+  la   $a0, str1
   syscall
-  li $v0, 5
+  li   $v0, 5
   syscall
-  sw $v0, n
+  sw   $v0, n         # Store n in memory
 
-  # read_int for n times
-  move $t0, $v0
-  la $t1, array
+  # Read n integers, storing them in array
+  move $t0, $v0      # $t0 = number of integers (n)
+  la   $t1, array   # pointer to array base
 gen_ints_0:
-  beq $t0, $zero, gen_ints_1
+  beq  $t0, $zero, gen_ints_1
 
-  # print_string(str2); read_int (--> $v0); array <<= $v0
-  li $v0, 4
-  la $a0, str2
+  # Prompt for each integer and read it
+  li   $v0, 4
+  la   $a0, str2
   syscall
-  li $v0, 5
+  li   $v0, 5
   syscall
-  sw $v0, ($t1)
+  sw   $v0, 0($t1)
 
   addi $t0, $t0, -1
   addi $t1, $t1, 4
-
-  beq $zero, $zero, gen_ints_0
+  j    gen_ints_0
 gen_ints_1:
 
-  # print_array(n, array)
-  lw $a0, n
-  la $a1, array
-  jal print_array
+  # Print original array
+  lw   $a0, n
+  la   $a1, array
+  jal  print_array
 
-  # sort_array(n, array)
-  lw $a0, n
-  la $a1, array
-  jal sort_array
-  
-  # print_array(n, array)
-  lw $a0, n
-  la $a1, array
-  jal print_array
+  # Call sort_array(n, array)
+  lw   $a0, n
+  la   $a1, array
+  jal  sort_array
 
-  # $ra = $s0
+  # Print sorted array
+  lw   $a0, n
+  la   $a1, array
+  jal  print_array
+
+  # Restore $ra from $s0 and return
   move $ra, $s0
- 
-  # return
-  jr $ra
-
+  jr   $ra
