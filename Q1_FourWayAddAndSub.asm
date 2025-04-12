6ad218b993b2629935ddd50a7dafcf13bd06bdb4
Seontae <hitu8931@yonsei.ac.kr>
2025-04-13 01:39:33 +0900

    a

--- Q1_FourWayAddAndSub.asm
+++ Q1_FourWayAddAndSub.asm
@@ -1,103 +1,95 @@
 .data
-newline:
-  .asciiz "\n"
-str0:
-  .asciiz "Enter a 32-bit signed integer (in decimal): "
-str1:
-  .asciiz "INFO: fourWayAddAndSub returned:\n"
-str2:
-  .asciiz "INFO:   $v0 = "
-str3:
-  .asciiz "INFO:   $v1 = "
-  
+newline:  .asciiz "\n"
+prompt:   .asciiz "Enter a 32-bit signed integer (in decimal): "
+info:     .asciiz "INFO: fourWayAddAndSub returned:\n"
+infoSum:  .asciiz "Sum = "
+infoDiff: .asciiz "Difference = "
+
 .text
-# $a0 ~ $a3: 32-bit signed integers
+# fourWayAddAndSub:
+#   계산: v0 = a0 + a1 + a2 + a3
+#         v1 = a0 - a1 - a2 - a3
 fourWayAddAndSub:
-  add   $t0, $a0, $a1
-  add   $t0, $t0, $a2
-  add   $v0, $t0, $a3
-  sub   $t1, $a0, $a1
-  sub   $t1, $t1, $a2
-  sub   $v1, $t1, $a3
-  jr    $ra
-  
+    # a1 + a2 + a3를 t0에 계산
+    add   $t0, $a1, $a2      # t0 = a1 + a2
+    add   $t0, $t0, $a3      # t0 = a1 + a2 + a3
+    # sum 계산: a0 + (a1+a2+a3)
+    add   $v0, $a0, $t0      # v0 = a0 + t0
+    # diff 계산: a0 - (a1+a2+a3)
+    sub   $v1, $a0, $t0      # v1 = a0 - t0
+    jr    $ra
+
 .globl main
 main:
-  li   $v0, 4
-  la   $a0, str0
-  syscall
-  li   $v0, 5
-  syscall
-  move $t0, $v0
+    # 첫 번째 정수 입력
+    li    $v0, 4
+    la    $a0, prompt
+    syscall
+    li    $v0, 5
+    syscall
+    move  $t0, $v0
 
-  li   $v0, 4
-  la   $a0, str0
-  syscall
-  li   $v0, 5
-  syscall
-  move $t1, $v0
+    # 두 번째 정수 입력
+    li    $v0, 4
+    la    $a0, prompt
+    syscall
+    li    $v0, 5
+    syscall
+    move  $t1, $v0
 
-  li   $v0, 4
-  la   $a0, str0
-  syscall
-  li   $v0, 5
-  syscall
-  move $t2, $v0
+    # 세 번째 정수 입력
+    li    $v0, 4
+    la    $a0, prompt
+    syscall
+    li    $v0, 5
+    syscall
+    move  $t2, $v0
 
-  li   $v0, 4
-  la   $a0, str0
-  syscall
-  li   $v0, 5
-  syscall
-  move $t3, $v0
+    # 네 번째 정수 입력
+    li    $v0, 4
+    la    $a0, prompt
+    syscall
+    li    $v0, 5
+    syscall
+    move  $t3, $v0
 
-  move $s0, $ra
-  move $s1, $a0
-  move $s2, $a1
-  move $s3, $a2
-  move $s4, $a3
+    # 함수 호출: 인자는 $a0 ~ $a3 에 전달
+    move  $a0, $t0
+    move  $a1, $t1
+    move  $a2, $t2
+    move  $a3, $t3
+    jal   fourWayAddAndSub
 
-  move $a0, $t0
-  move $a1, $t1
-  move $a2, $t2
-  move $a3, $t3
-  jal fourWayAddAndSub
+    # 결과값을 각각 t4 (sum)와 t5 (diff)에 저장
+    move  $t4, $v0   # t4 <- sum
+    move  $t5, $v1   # t5 <- difference
 
-  move $ra, $s0
-  move $a0, $s1
-  move $a1, $s2
-  move $a2, $s3
-  move $a3, $s4
+    # 결과 메시지 출력
+    li    $v0, 4
+    la    $a0, info
+    syscall
 
-  move $t0, $v0
-  move $t1, $v1
+    # sum 출력
+    li    $v0, 4
+    la    $a0, infoSum
+    syscall
+    li    $v0, 1
+    move  $a0, $t4
+    syscall
+    li    $v0, 4
+    la    $a0, newline
+    syscall
 
-  li   $v0, 4
-  la   $a0, str1
-  syscall
+    # difference 출력
+    li    $v0, 4
+    la    $a0, infoDiff
+    syscall
+    li    $v0, 1
+    move  $a0, $t5
+    syscall
+    li    $v0, 4
+    la    $a0, newline
+    syscall
 
-  li   $v0, 4
-  la   $a0, str2
-  syscall
-  li   $v0, 1
-  move $a0, $t0
-  syscall
-  li   $v0, 4
-  la   $a0, newline
-  syscall
-
-  li   $v0, 4
-  la   $a0, str3
-  syscall
-  li   $v0, 4
-  la   $a0, str2
-  syscall
-  li   $v0, 1
-  move $a0, $t1
-  syscall
-  li   $v0, 4
-  la   $a0, newline
-  syscall
-
-  li   $v0, 0
-  jr   $ra
+    li    $v0, 0
+    jr    $ra
--- Q4_SortArray.asm
+++ Q4_SortArray.asm
@@ -1,102 +1,92 @@
 .data
-str1:
-  .asciiz "Enter the number of integers to sort: "
-str2:
-  .asciiz "Enter a 32-bit integer (in decimal): "
-newline:
-  .asciiz "\n"
-space:
-  .asciiz " "
-n:
-  .align 2
-  .space 4
-array:
-  .align 2
-  .space 4096
-  
+promptCount: .asciiz "Enter the number of integers to sort: "
+promptInt:   .asciiz "Enter a 32-bit integer (in decimal): "
+nl:          .asciiz "\n"
+sp:          .asciiz " "
+n:           .space 4
+array:       .space 4096
+
 .text
-# $a0 = n, $a1 = &array[0]
 print_array:
-  move $t0, $a0
-  li   $t1, 0
-  move $t2, $a1
-print_array_loop:
-  beq $t1, $t0, print_array_return
-  li  $v0, 1
-  lw  $a0, ($t2)
-  syscall
-  li  $v0, 4
-  la  $a0, space
-  syscall
-  addi $t1, $t1, 1
-  addi $t2, $t2, 4
-  j   print_array_loop
-print_array_return:
-  li  $v0, 4
-  la  $a0, newline
-  syscall
-  jr  $ra
-  
-# $a0 = n, $a1 = &array[0]
+    move  $t0, $a0
+    li    $t1, 0
+    move  $t2, $a1
+pa_loop:
+    beq   $t1, $t0, pa_done
+    li    $v0, 1
+    lw    $a0, 0($t2)
+    syscall
+    li    $v0, 4
+    la    $a0, sp
+    syscall
+    addi  $t1, $t1, 1
+    addi  $t2, $t2, 4
+    j     pa_loop
+pa_done:
+    li    $v0, 4
+    la    $a0, nl
+    syscall
+    jr    $ra
+
 sort_array:
-  move $t2, $a0
-  li   $t0, 0
+    move  $s0, $a0       # s0 = n (정수 개수)
+    li    $t0, 0         # i = 0
 outer_loop:
-  sub  $t3, $t2, 1
-  bge  $t0, $t3, sort_end
-  li   $t1, 0
+    sub   $t1, $s0, 1
+    bge   $t0, $t1, sort_done
+    li    $t2, 0         # j = 0
 inner_loop:
-  sub  $t4, $t2, $t0
-  addi $t4, $t4, -1
-  bge  $t1, $t4, inner_end
-  sll  $t5, $t1, 2
-  add  $t5, $a1, $t5
-  lw   $t6, 0($t5)
-  addi $t7, $t1, 1
-  sll  $t7, $t7, 2
-  add  $t7, $a1, $t7
-  lw   $t8, 0($t7)
-  ble  $t6, $t8, no_swap
-  sw   $t8, 0($t5)
-  sw   $t6, 0($t7)
+    sub   $t3, $s0, $t0
+    addi  $t3, $t3, -1
+    bge   $t2, $t3, next_outer
+    sll   $t4, $t2, 2
+    add   $t4, $a1, $t4   # 주소: array[j]
+    lw    $t5, 0($t4)     # array[j]
+    addi  $t6, $t2, 1
+    sll   $t6, $t6, 2
+    add   $t6, $a1, $t6   # 주소: array[j+1]
+    lw    $t7, 0($t6)     # array[j+1]
+    ble   $t5, $t7, no_swap
+    sw    $t7, 0($t4)
+    sw    $t5, 0($t6)
 no_swap:
-  addi $t1, $t1, 1
-  j inner_loop
-inner_end:
-  addi $t0, $t0, 1
-  j outer_loop
-sort_end:
-  jr  $ra
-  
+    addi  $t2, $t2, 1
+    j     inner_loop
+next_outer:
+    addi  $t0, $t0, 1
+    j     outer_loop
+sort_done:
+    jr    $ra
+
 .globl main
 main:
-  li   $v0, 4
-  la   $a0, str1
-  syscall
-  li   $v0, 5
-  syscall
-  sw   $v0, n
-  move $t0, $v0
-  la   $t1, array
-gen_ints_0:
-  beq  $t0, $zero, gen_ints_1
-  li   $v0, 4
-  la   $a0, str2
-  syscall
-  li   $v0, 5
-  syscall
-  sw   $v0, ($t1)
-  addi $t0, $t0, -1
-  addi $t1, $t1, 4
-  j gen_ints_0
-gen_ints_1:
-  lw   $a0, n
-  la   $a1, array
-  jal  print_array
-  lw   $a0, n
-  la   $a1, array
-  jal  sort_array
-  lw   $a0, n
-  la   $a1, array
-  jal  print_array
-  jr  $ra
+    li    $v0, 4
+    la    $a0, promptCount
+    syscall
+    li    $v0, 5
+    syscall
+    sw    $v0, n       # 저장된 n
+    move  $t0, $v0     # 남은 개수
+    la    $t1, array
+read_loop:
+    beq   $t0, $zero, sort_phase
+    li    $v0, 4
+    la    $a0, promptInt
+    syscall
+    li    $v0, 5
+    syscall
+    sw    $v0, 0($t1)
+    addi  $t0, $t0, -1
+    addi  $t1, $t1, 4
+    j     read_loop
+sort_phase:
+    lw    $a0, n
+    la    $a1, array
+    jal   print_array
+    lw    $a0, n
+    la    $a1, array
+    jal   sort_array
+    lw    $a0, n
+    la    $a1, array
+    jal   print_array
+    jr    $ra
